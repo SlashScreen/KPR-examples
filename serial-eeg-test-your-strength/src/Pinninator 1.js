cfefe95dfe422e2222ec9b5ab0f1d6d2183ad808
Greyson Jennings <TALLESTTOWER1@GMAIL.COM>
2015-05-13 21:29:33 -0700

    Almost done with the BLL, just need to do more functions.

--- serial-eeg-test-your-strength/src/Pinninator 1.js
+++ serial-eeg-test-your-strength/src/Pinninator 1.js
@@ -1,6 +1,10 @@
 //@module
-exports.pins={}
+exports.pins = {
+	out:{type:"Digital", direction:"out" , baud:9600}
+}
+exports.configure=function(parameters){
+}
 this.duration = exports.pins.duration
 out.write(1);
-sensorUtil.mdelay(duration);
+sensorUtils.mdelay(duration);
 out.write(0)
